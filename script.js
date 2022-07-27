b24b8d0579580df5338c16ad0288298ac524c1bd
NABIL ADIB <nabil298adib@gmail.com>
2022-07-27 22:57:28 +0800

    COMMIT: filtering out the image of the recipe
    Learn the basic of array data structure and accesing it from API

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 
-const recipeCard = document.querySelector(".recipe__card");
+//TODO: getNameRecipe();
+//FIXME:Cannot accesing the DOM
+//FIXME:Failed to display name in h4 attribute
+const apiKey = "b8c5e15c587342d0b71f4810901d9384";
 const getNameRecipe = () => {
   fetch(
     `https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=false`
   )
     .then((response) => response.json())
     .then((recipeInfo) => {
-      console.log(recipeInfo.title)
-      document.querySelector(".recipe__card").innerHTML = `<img src="${recipeInfo.image}" height
-      =200 width=100/>`
+      console.log(recipeInfo.title);
+      document.querySelector(".recipe__name").innerText = recipeInfo.title;
     });
 };
+const searchBar = document.getElementById("searchIcon");
+searchBar.addEventListener("click", getNameRecipe());
 
-getNameRecipe();
+//TODO:Filtering out the recipe
+
+const getListOfRecipe = () => {
+  const recipe = fetch(`https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=false`);
+  recipe.then(recipeObject => recipeObject.json()).then(recipeObject => {
+    //FIXME:DATA STRUCTURE ARRAY
+    //TODO:Get the images of the recipe
+    let imageOfRecipe = [];
+    for (let i = 0; i < recipeObject.extendedIngredients.length; i++) {
+      imageOfRecipe.push(recipeObject.extendedIngredients[i].image);
+    }
+    // console.log(imageOfRecipe);
+     // console.log(recipeObject.extendedIngredients[0].image);
+  }
+  
+  );
+ 
+}
+getListOfRecipe();
+
+
